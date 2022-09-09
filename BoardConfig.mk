412c090cd7455250ef6b7911c938cb366f1f6d25
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-09 21:19:47 +0800

    vince: Remove metadata partition
    None of public vince rom is using metadata partition right now, they don't know what's it and they'll complain metadata fails to mount

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,5 @@ DEVICE_PATH := device/xiaomi/vince
 # Kernel
 TARGET_PREBUILT_KERNEL := device/xiaomi/kernel-mithorium/vince/Image.gz-dtb
 
-# Partitions
-BOARD_USES_METADATA_PARTITION := true
-
 # Recovery
 TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
